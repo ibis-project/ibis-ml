7b045b9d2eb2356647787ae93dadf38a301fcf98
Jim Crist-Harif <jcristharif@gmail.com>
2023-08-23 17:19:31 -0500

    Document ibisml.steps.temporal

--- ibisml/steps/temporal.py
+++ ibisml/steps/temporal.py
@@ -10,6 +10,41 @@ from ibisml.select import SelectionType, selector
 
 
 class ExpandDate(Step):
+    """A step for expanding date columns into one or more features.
+
+    New features will be named ``{input_column}_{component}``. For example, if
+    expanding a ``"year"`` component from column ``"x"``, the feature column
+    would be named ``"x_year"``.
+
+    Parameters
+    ----------
+    inputs
+        A selection of date columns to expand into new features.
+    components
+        A sequence of components to expand. Options include
+
+        - ``day``: the day of the month as a numeric value
+        - ``week``: the week of the year as a numeric value
+        - ``month``: the month of the year as a categorical value
+        - ``year``: the year as a numeric value
+        - ``dow``: the day of the week as a categorical value
+        - ``doy``: the day of the year as a numeric value
+
+        Defaults to ``["dow", "month", "year"]``.
+
+    Examples
+    --------
+    >>> import ibisml as ml
+
+    Expand date columns using the default components
+
+    >>> step = ml.ExpandDate(ml.date())
+
+    Expand specific columns using specific components
+
+    >>> step = ml.ExpandDate(["x", "y"], ["day", "year"])
+    """
+
     def __init__(
         self,
         inputs: SelectionType,
@@ -65,6 +100,35 @@ class ExpandDate(Step):
 
 
 class ExpandTime(Step):
+    """A step for expanding time columns into one or more features.
+
+    New features will be named ``{input_column}_{component}``. For example, if
+    expanding an ``"hour"`` component from column ``"x"``, the feature column
+    would be named ``"x_hour"``.
+
+    Parameters
+    ----------
+    inputs
+        A selection of time columns to expand into new features.
+    components
+        A sequence of components to expand. Options include ``hour``,
+        ``minute``, ``second``, and ``millisecond``.
+
+        Defaults to ``["hour", "minute", "second"]``.
+
+    Examples
+    --------
+    >>> import ibisml as ml
+
+    Expand time columns using the default components
+
+    >>> step = ml.ExpandTime(ml.time())
+
+    Expand specific columns using specific components
+
+    >>> step = ml.ExpandTime(["x", "y"], ["hour", "minute"])
+    """
+
     def __init__(
         self,
         inputs: SelectionType,
