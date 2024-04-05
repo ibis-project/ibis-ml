3e649063a313356666b21908c78cb1d708dd44b2
Jiting Xu <126802425+jitingxu1@users.noreply.github.com>
2024-04-05 11:44:28 -0700

    feat(steps): implement zero-variance filter (#48)

--- ibisml/steps/__init__.py
+++ ibisml/steps/__init__.py
@@ -1,5 +1,6 @@
 from ibisml.steps.common import Cast, Drop, Mutate, MutateAt
 from ibisml.steps.encode import CategoricalEncode, CountEncode, OneHotEncode
+from ibisml.steps.feature_selection import ZeroVariance
 from ibisml.steps.impute import FillNA, ImputeMean, ImputeMedian, ImputeMode
 from ibisml.steps.standardize import ScaleMinMax, ScaleStandard
 from ibisml.steps.temporal import ExpandDate, ExpandDateTime, ExpandTime
@@ -21,4 +22,5 @@ __all__ = (
     "OneHotEncode",
     "ScaleMinMax",
     "ScaleStandard",
+    "ZeroVariance",
 )
