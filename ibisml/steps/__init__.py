f64d37786d62de1065620e0442818bc065420431
Jiting Xu <126802425+jitingxu1@users.noreply.github.com>
2024-04-16 10:49:04 -0700

    univariate outlier detection

--- ibisml/steps/__init__.py
+++ ibisml/steps/__init__.py
@@ -2,6 +2,7 @@ from ibisml.steps.common import Cast, Drop, Mutate, MutateAt
 from ibisml.steps.encode import CategoricalEncode, CountEncode, OneHotEncode
 from ibisml.steps.feature_selection import ZeroVariance
 from ibisml.steps.impute import FillNA, ImputeMean, ImputeMedian, ImputeMode
+from ibisml.steps.outlier import UnivariateOutlier
 from ibisml.steps.standardize import ScaleMinMax, ScaleStandard
 from ibisml.steps.temporal import ExpandDate, ExpandDateTime, ExpandTime
 
@@ -22,5 +23,6 @@ __all__ = (
     "OneHotEncode",
     "ScaleMinMax",
     "ScaleStandard",
+    "UnivariateOutlier",
     "ZeroVariance",
 )
