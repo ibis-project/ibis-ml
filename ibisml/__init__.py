49810fdaca4504f0886eec975bced2742f96a5c9
Deepyaman Datta <deepyaman.datta@utexas.edu>
2024-04-26 15:40:30 -0700

    feat(examples): add model picker and support torch

--- ibisml/__init__.py
+++ ibisml/__init__.py
@@ -23,3 +23,22 @@ from ibisml.select import (
 from ibisml.steps import *
 
 from ._version import __version__
+
+
+def _auto_patch_skorch() -> None:
+    import ibis.expr.types as ir
+    import numpy as np
+    import skorch.net
+
+    old_fit = skorch.net.NeuralNet.fit
+
+    def fit(self, X, y=None, **fit_params):
+        if isinstance(y, ir.Column):
+            y = np.asarray(y)
+
+        return old_fit(self, X, y, **fit_params)
+
+    skorch.net.NeuralNet.fit = fit
+
+
+_auto_patch_skorch()
