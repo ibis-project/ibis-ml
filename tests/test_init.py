9e3e466c6e03c8342b526308528039ec40e348c2
Jiting Xu <126802425+jitingxu1@users.noreply.github.com>
2024-09-12 19:33:58 +0000

    add test optional dependency non-blocking
    add test_import IbisML

---
+++ tests/test_init.py
@@ -0,0 +1,26 @@
+import importlib
+import sys
+from importlib import import_module, reload
+from unittest import mock
+
+import pytest
+
+
+@pytest.mark.parametrize("optional_dep", ["pandas", "numpy", "pyarrow"])
+def test_optional_dependencies(optional_dep, caplog):
+    with mock.patch.dict(sys.modules):
+        sys.modules[optional_dep] = None
+        if "ibis_ml" in sys.modules:
+            reload(sys.modules["ibis_ml"])
+        else:
+            import_module("ibis_ml")
+
+        assert "ibis_ml" in sys.modules
+
+
+def test_import():
+    try:
+        importlib.import_module("ibis_ml")
+    except AttributeError:
+        raise ImportError("cannot import IbisML") from None
+    assert "ibis_ml" in sys.modules
