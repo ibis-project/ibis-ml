6475e26c851d04d5b556dddbb9e79a7e96d448cf
Deepyaman Datta <deepyaman.datta@utexas.edu>
2024-09-13 15:33:52 -0600

    test: fix given parametrization and simplify logic

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,16 +1,12 @@
 import sys
-from importlib import import_module, reload
-from unittest import mock
+from unittest.mock import patch
 
 import pytest
 
 
-@pytest.mark.parametrize("optional_dep", ["pandas", "numpy", "pyarrow"])
-def test_optional_dependencies(optional_dep):
-    with mock.patch.dict(sys.modules, {"optional_dependency": None}):
-        if "ibis_ml" in sys.modules:
-            reload(sys.modules["ibis_ml"])
-        else:
-            import_module("ibis_ml")
-
-        assert "ibis_ml" in sys.modules
+# https://stackoverflow.com/a/65034142/1093967
+@pytest.mark.parametrize("optional_dependency", ["pandas", "numpy", "pyarrow"])
+def test_without_dependency(optional_dependency):
+    with patch.dict(sys.modules, {optional_dependency: None}):
+        assert "ibis_ml" not in sys.modules
+        import ibis_ml  # noqa: F401
