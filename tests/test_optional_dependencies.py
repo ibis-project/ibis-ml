5f88d12aa22ac91eebcab55f36ba51515db16920
Deepyaman Datta <deepyaman.datta@utexas.edu>
2024-09-13 15:42:00 -0600

    test: use `import_module` instead of pure `import`

--- tests/test_optional_dependencies.py
+++ tests/test_optional_dependencies.py
@@ -1,12 +1,13 @@
 import sys
+from importlib import import_module
 from unittest.mock import patch
 
 import pytest
 
 
 # https://stackoverflow.com/a/65034142/1093967
-@pytest.mark.parametrize("optional_dependency", ["pandas", "numpy", "pyarrow"])
+@pytest.mark.parametrize("optional_dependency", ["numpy", "pandas", "pyarrow"])
 def test_without_dependency(optional_dependency):
     with patch.dict(sys.modules, {optional_dependency: None}):
         assert "ibis_ml" not in sys.modules
-        import ibis_ml  # noqa: F401
+        import_module("ibis_ml")
