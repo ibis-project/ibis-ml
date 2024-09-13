6c7f9f15e952ea0a43684d0df0cc7e55d3ed9224
Deepyaman Datta <deepyaman.datta@utexas.edu>
2024-09-13 15:54:49 -0600

    fix: revert approach for testing removal of module

--- tests/test_optional_dependencies.py
+++ tests/test_optional_dependencies.py
@@ -1,13 +1,15 @@
 import sys
-from importlib import import_module
+from importlib import import_module, reload
 from unittest.mock import patch
 
 import pytest
 
 
-# https://stackoverflow.com/a/65034142/1093967
+# https://stackoverflow.com/a/65163627
 @pytest.mark.parametrize("optional_dependency", ["numpy", "pandas", "pyarrow"])
 def test_without_dependency(optional_dependency):
     with patch.dict(sys.modules, {optional_dependency: None}):
-        assert "ibis_ml" not in sys.modules
-        import_module("ibis_ml")
+        if "ibis_ml" in sys.modules:
+            reload(sys.modules["ibis_ml"])
+        else:
+            import_module("ibis_ml")
